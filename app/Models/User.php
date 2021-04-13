7773ddba85533767fb20bb562727babf661b0454
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-04-13 22:51:33 +0700

    Update model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,13 +10,15 @@ class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
 
+    protected $table = 'user';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'username', 'password', 'role',
+        'username', 'password', 'foto_profil', 'role'
     ];
 
     /**
@@ -27,13 +29,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    // protected $casts = [
-    //     'email_verified_at' => 'datetime',
-    // ];
 }
