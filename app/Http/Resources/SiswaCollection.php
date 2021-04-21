6c1137475b029ff5b112da59d17d3f0202226cbf
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-04-21 19:05:12 +0700

    siswacollection

---
+++ app/Http/Resources/SiswaCollection.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SiswaCollection extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'data_of' => $this->data_of,
+            'user' => new UserCollection($this->whenLoaded('user')),
+            'nisn' => $this->NISN,
+            'name' => $this->nama,
+            'sex' => $this->jk == 'l' ? 'Male' : 'Female',
+            'whatsapp' => $this->whatsapp,
+            'address' => $this->alamat,
+            'birth_date' => $this->tanggal_lahir,
+            'student_pict' => $this->foto_siswa,
+            'class' => new KelasCollection($this->whenLoaded('class')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
