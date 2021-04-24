d0d1f60f2e905c429d9e9ef08fd9a17a9d8fb158
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-04-24 15:37:48 +0700

    menambah method teacher dan mapel pada model pivot Guru_Mapel

--- app/Models/Guru_Mapel.php
+++ app/Models/Guru_Mapel.php
@@ -8,4 +8,14 @@ class Guru_Mapel extends Model
 {
     protected $table = 'guru_mapel';
     protected $fillable = ['id_guru', 'id_mapel'];
+
+    public function teacher()
+    {
+        return $this->belongsTo(Guru::class, 'id_guru');
+    }
+
+    public function mapel()
+    {
+        return $this->belongsTo(Mapel::class, 'id_mapel');
+    }
 }
