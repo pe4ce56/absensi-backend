82d707950184c9f4a6a4039866af4a19d128875b
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-04-25 12:25:02 +0700

    menambah nullable pada kolom keterangan dan status pada tb absensi

--- database/migrations/2021_04_12_051352_create_absensi_table.php
+++ database/migrations/2021_04_12_051352_create_absensi_table.php
@@ -21,8 +21,8 @@ class CreateAbsensiTable extends Migration
             $table->foreign('id_jadwal')->references('id')->on('jadwal')->cascadeOnDelete();
             $table->timestamp('waktu');
             $table->string('lokasi');
-            $table->string('keterangan');
-            $table->enum('status',['diterima, ditolak']);
+            $table->string('keterangan')->nullable();
+            $table->enum('status',['diterima, ditolak'])->nullable();
             $table->timestamps();
         });
     }
