0139ac26cb7a9f74eecd686608a3b403ae1e4903
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-04-25 12:20:03 +0700

    Profile controller guru

---
+++ app/Http/Controllers/API/Guru/ProfileController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API\Guru;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+
+class ProfileController extends Controller
+{
+    //
+}
